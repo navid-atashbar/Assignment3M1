dcd509756e31de82bb7c159b6a2988ecdad76cbb
TawannAlvarez <tawann@lazymonkey.net>
2026-02-28 06:44:12 +0000

    final checks

--- maina2.py
+++ maina2.py
@@ -1,16 +1,6 @@
-from searcher import Searcher
-import time
-
+from user_interface import run
 def main():
-
-    searcher = Searcher()
-    start_time = time.time()
-    lists = searcher.search("artificial intelligence")
-    print(f"Load time {time.time() - start_time} seconds")
-    new_start_time = time.time()
-    print(searcher.id_to_links(lists))
-    print(f"Search time {time.time() - new_start_time} seconds")
-    print(f"Took {time.time() - start_time} seconds")
+    run()
 
 
 if __name__ == "__main__":
