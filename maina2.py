7830fb7aff043a7ee9f0a46d9ff5aa7a138c4d9d
TawannAlvarez <tawann@lazymonkey.net>
2026-02-28 01:02:11 +0000

    put main back for testing purposes

--- maina2.py
+++ maina2.py
@@ -1,8 +1,16 @@
-from user_interface import run
+from searcher import Searcher
+import time
 
 def main():
 
-    run()
+    searcher = Searcher()
+    start_time = time.time()
+    lists = searcher.search("artificial intelligence")
+    print(f"Load time {time.time() - start_time} seconds")
+    new_start_time = time.time()
+    print(searcher.id_to_links(lists))
+    print(f"Search time {time.time() - new_start_time} seconds")
+    print(f"Took {time.time() - start_time} seconds")
 
 
 if __name__ == "__main__":
