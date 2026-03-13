a9a1e178ea4c972e6eb1871550bf067ea9b6ce0e
navida2 <atashbar@uci.edu>
2026-03-13 13:47:56 -0700

    finished the website ec

--- website_ec.py
+++ website_ec.py
@@ -9,6 +9,43 @@ HTML = """
 <html lang="en">
 <head><title>Search</title></head>
 <body>
-    <h1>Search</h1>
+    <h1>Zotgle</h1>
     <form action="/search" method="GET">
-        <input type="Text" value="Search">"""
+        <input type="Text" name="q" placeholder="Enter search query" value="{{ query }}">
+        <button type="submit">Search</button>
+    </form>
+    {% if results is not none %}
+        <p> Here are the top 5 results in {{time}} secs</p>
+        {% for url,score in results %}
+            <p> {{loop.index}}. <a href="{{ url }}">{{ url }}</a> (score: {{ score }})</p>
+        {% endfor %}
+        {% if not results %}
+            <p> Found NO results</p>
+        {% endif %}
+    {% endif %}
+    
+    </body>
+    </html>
+        
+        
+        
+        
+        
+        
+    """
+@app.route("/")
+def index():
+    return render_template_string(HTML)
+@app.route("/search")
+def search():
+    q = request.args.get("q", "").strip()
+    if not q:
+        return render_template_string(HTML)
+    start = time.time()
+    results = searcher.search(q)
+    elapsed = f"{time.time() - start}"
+    return render_template_string(HTML,query="", results=results, elapsed=elapsed)
+if __name__ == "__main__":
+    app.run(debug=True)
+#TO RUN THIS RUN THIS FILE AND IN YOUR BROWSER PASTE THIS
+# http://127.0.0.1:5000
